5a93133b44e63ba440cc71d701fbe872fd1a1d9e
Oscar Wolter <Oscardawolter@gmail.com>
2017-10-30 12:05:50 +0100

    Added AgentClass with subclasses added ItemClass with subclasses

---
+++ Lab6/Lab6/Agent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class AgentClass
+    {
+        private string Name { get; set; }
+
+        public AgentClass(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
